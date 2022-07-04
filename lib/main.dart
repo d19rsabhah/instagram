c05e52518aaf1a2bcfa4bf752ec32aa385b92637
Rishav Das <88232725+d19rsabhah@users.noreply.github.com>
2022-07-04 16:38:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,6 @@ void main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-          // apiKey: "AIzaSyCZ-xrXqD5D19Snauto-Fx_nLD7PLrBXGM",
-          // appId: "1:585119731880:web:eca6e4b3c42a755cee329d",
-          // messagingSenderId: "585119731880",
-          // projectId: "instagram-clone-4cea4",
-          // storageBucket: 'instagram-clone-4cea4.appspot.com'
-
         //  apiKey: "AIzaSyD6tGQhQYcjhTRAGVzW3C2pSqhV_e9Yp_M",
           projectId: "instagram-tut-98067",
           storageBucket: "instagram-tut-98067.appspot.com",
